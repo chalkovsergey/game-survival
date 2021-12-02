56ffde108c52fe2a328ed57fe7f1f6e5612d29c8
Sergey <sergejcalkov222@gmail.com>
2021-12-02 20:00:56 +0500

    Main Survival
    Main Survival

---
+++ SURVIVAL PC Edition/Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,113 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public GameObject UIBG; // renamed
+    public GameObject crosshair;
+    public Transform inventoryPanel;
+    public List<InventorySlot> slots = new List<InventorySlot>();
+    public bool isOpened;
+    public float reachDistance = 3f;
+    private Camera mainCamera;
+    public CinemachineVirtualCamera CVC;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        UIBG.SetActive(true);
+    }
+    void Start()
+    {
+        mainCamera = Camera.main;
+        for (int i = 0; i < inventoryPanel.childCount; i++)
+        {
+            if (inventoryPanel.GetChild(i).GetComponent<InventorySlot>() != null)
+            {
+                slots.Add(inventoryPanel.GetChild(i).GetComponent<InventorySlot>());
+            }
+        }
+        UIBG.SetActive(false);
+        inventoryPanel.gameObject.SetActive(false);//new line
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            isOpened = !isOpened;
+            if (isOpened)
+            {
+                UIBG.SetActive(true);
+                inventoryPanel.gameObject.SetActive(true); // new line
+                crosshair.SetActive(false);
+                CVC.GetCinemachineComponent<CinemachinePOV>().m_HorizontalAxis.m_InputAxisName = "";
+                CVC.GetCinemachineComponent<CinemachinePOV>().m_VerticalAxis.m_InputAxisName = "";
+                CVC.GetCinemachineComponent<CinemachinePOV>().m_HorizontalAxis.m_InputAxisValue = 0;
+                CVC.GetCinemachineComponent<CinemachinePOV>().m_VerticalAxis.m_InputAxisValue = 0;
+                // Прекрепляем курсор к середине экрана
+                Cursor.lockState = CursorLockMode.None;
+                // и делаем его невидимым
+                Cursor.visible = true;
+
+            }
+            else
+            {
+                UIBG.SetActive(false);
+                inventoryPanel.gameObject.SetActive(false); // new line
+                crosshair.SetActive(true);
+                CVC.GetCinemachineComponent<CinemachinePOV>().m_HorizontalAxis.m_InputAxisName = "Mouse X";
+                CVC.GetCinemachineComponent<CinemachinePOV>().m_VerticalAxis.m_InputAxisName = "Mouse Y";
+                // Прекрепляем курсор к середине экрана
+                Cursor.lockState = CursorLockMode.Locked;
+                // и делаем его невидимым
+                Cursor.visible = false;
+            }
+        }
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (Physics.Raycast(ray, out hit, reachDistance))
+            {
+                if (hit.collider.gameObject.GetComponent<Item>() != null)
+                {
+                    AddItem(hit.collider.gameObject.GetComponent<Item>().item, hit.collider.gameObject.GetComponent<Item>().amount);
+                    Destroy(hit.collider.gameObject);
+                }
+            }
+        }
+    }
+    private void AddItem(ItemScriptableObject _item, int _amount)
+    {
+        foreach (InventorySlot slot in slots)
+        {
+            // В слоте уже имеется этот предмет
+            if (slot.item == _item)
+            {
+                if (slot.amount + _amount <= _item.maximumAmount)
+                {
+                    slot.amount += _amount;
+                    slot.itemAmountText.text = slot.amount.ToString();
+                    return;
+                }
+                break;
+            }
+        }
+        foreach (InventorySlot slot in slots)
+        {
+            if (slot.isEmpty == true)
+            {
+                slot.item = _item;
+                slot.amount = _amount;
+                slot.isEmpty = false;
+                slot.SetIcon(_item.icon);
+                slot.itemAmountText.text = _amount.ToString();
+                break;
+            }
+        }
+    }
+}
